7b0fe5ba07b7dacfebceb3e012f0e1b506aecfdd
alkamaurya12 <54631196+alkamaurya12@users.noreply.github.com>
2021-03-11 14:49:28 +0530

    Update Pig.js

--- Pig.js
+++ Pig.js
@@ -1,7 +1,7 @@
 class Pig extends BaseClass {
     constructor(x, y) {
      super (x,y, 50, 50)
-     this.image = loadImage("sprites/pig.jpg")
+     this.image = loadImage("pig.jpg")
     }
   }
   
